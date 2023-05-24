48319a3282cbdecc43dcb40676daa87d4995b784
Joe C <consolinojoe@gmail.com>
2023-05-24 17:28:02 -0400

    Finished the homepage draft

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Platter1 from '../../Assets/Platter1.jpg';
 import Platter2 from '../../Assets/Platter2.jpg';
+import Platter3 from '../../Assets/Platter3.jpg'
 import './Home.css';
 
 const Home = () => {
     return (
         <div className='homeContainer'>
             <div className='imageContainer'>
-                <img className='cheesePlatterImg' src={Platter1} alt='A Cheese Platter' />
+                <img className='cheesePlatterImg' src={Platter2} alt='A Cheese Platter' />
             </div>
             <div className='textContainer'>
                 <h1>Welcome</h1>
@@ -19,10 +21,28 @@ const Home = () => {
             </div>
 
             <div className='textContainer'>
-                <p className='welcomeText'>Hello all you classy people! </p>
+                <p className='welcomeText'>Now if you're anything like me you enjoy eating chunks of dairy paired with a nice meat on a tasty cracker.
+                It's one thing to take it out of the fridge for a personal snack, but what happens when you have guests over? Laying it out on a platter
+                is a terrific idea, but sometimes one that is easier said than done. Or maybe you bought all the charcuterie your heart desires only
+                to realize you can't fit it all? Well, before you get your prosciutto all in a bunch give the virtual build your own board a try! It's
+                made to make your visual planning easier, so you can spend more time eating! </p>
             </div>
             <div className='imageContainer'>
-                <img className='cheesePlatterImg' src={Platter2} alt='Another Cheese Platter' />
+                <img className='cheesePlatterImg' src={Platter1} alt='Another Cheese Platter' />
+            </div>
+            <div className='imageAndButtonContainer'>
+                <div className='imageContainer'>
+                    <img className='cheesePlatterImg' src={Platter3} alt='A third Cheese Platter' />
+                </div>
+                <div className='textContainer'>
+                <Link to='/build'>
+                     <button 
+                        className='buildButton'
+                        >
+                            Build A Board!
+                    </button>
+                </Link>
+                </div>
             </div>
         </div>
     );
