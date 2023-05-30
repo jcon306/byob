64d69568422e7bfeca0cd001e3fe5a58c9e9f985
Joe C <consolinojoe@gmail.com>
2023-05-30 09:35:44 -0400

    Working on the drag and drop functionality of the board

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,17 +1,5 @@
-// import React from 'react'
-// import './Board.css'
-
-// const Board = () => {
-//     return (
-//         <div>
-//             <div className='defaultBoard'></div>
-//         </div>
-//     )
-// }
-
-// export default Board
-
 import React from 'react';
+import defaultBoard from '../../Assets/defaultBoard.jpg'
 import './Board.css'
 
 const Board = () => {
@@ -19,16 +7,30 @@ const Board = () => {
     event.preventDefault();
     const item = event.dataTransfer.getData('text/plain');
     const draggedImage = document.getElementById(item);
-    const clonedImage = draggedImage.cloneNode(true);
-    event.target.appendChild(clonedImage);
+  
+    // Create a new image element
+    const droppedImage = new Image();
+    droppedImage.src = draggedImage.src;
+    droppedImage.className = 'droppedImage';
+  
+    // Append the dropped image to the event target
+    event.target.appendChild(droppedImage);
   };
+  
 
   const handleDragOver = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div className='defaultBoard' onDrop={handleDrop} onDragOver={handleDragOver}>
+    <div>
+      <img 
+        className='defaultBoard'
+        src={defaultBoard}
+        alt='Empty Cheese Board'
+        onDrop={handleDrop} 
+        onDragOver={handleDragOver}>
+      </img>  
     </div>
   );
 };
