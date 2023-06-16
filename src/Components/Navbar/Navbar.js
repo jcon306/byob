a2246adb40b58da74be3076a14a5ea6b21cc3b51
Joe C <consolinojoe@gmail.com>
2023-06-16 11:55:15 -0400

    Small updates

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -23,6 +23,7 @@ const [isMenuOpen, setIsMenuOpen] = useState(false);
             <ul>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/build'>Build</Link></li>
+            <li><Link to='/login'>Log In</Link></li>
             </ul>
         </div>
       </div>
---
+++ src/LogIn/Login.js
@@ -0,0 +1,7 @@
+import './Login.css'
+
+const Login = () => {
+
+}
+
+export default Login
